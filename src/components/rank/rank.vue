ed2c947c1070611753f39a89c81c300c49ecf84e
deryabuder <904921115@qq.com>
2018-07-21 16:24:28 +0800

    Header

---
+++ src/components/rank/rank.vue
@@ -0,0 +1,12 @@
+<template>
+<div>排行</div>
+</template>
+
+<script>
+export default {
+  name: 'Rank'
+}
+</script>
+
+<style lang='stylus' scoped>
+</style>
---
+++ src/components/singer/singer.vue
@@ -0,0 +1,12 @@
+<template>
+<div>歌手</div>
+</template>
+
+<script>
+export default {
+  name: 'Singer'
+}
+</script>
+
+<style lang='stylus' scoped>
+</style>
